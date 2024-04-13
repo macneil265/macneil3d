20d81c38d71dd588cdc0eff2382a91c4bbf1f1c9
Mac Neil <33123884+macneil265@users.noreply.github.com>
2024-04-13 21:34:19 +0200

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter } from "react-router-dom"
+
+import {Hero, Navbar} from './components'
+
 const App = () => {
 
   return (
-    <div>
-      Developer Portfoliow
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar/>
+          <Hero/>
+
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
