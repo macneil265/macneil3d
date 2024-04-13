cdf33583ed061363b2539acecbcff29304a07b93
Mac Neil <33123884+macneil265@users.noreply.github.com>
2024-04-13 22:48:22 +0200

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
 
-import {Hero, Navbar} from './components'
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
 
@@ -10,7 +10,15 @@ const App = () => {
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar/>
           <Hero/>
-
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className="relative z-0">
+          <Contact/>
+          <StarsCanvas/>
         </div>
       </div>
     </BrowserRouter>
